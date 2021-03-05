e933b253be4b3728a4e2d2cba0c369f060915d66
Mohak Sharma <mohak327@gmail.com>
2021-03-05 13:35:13 +0530

    fxn error
    Plane() is not working and causing the cube to disappear.

--- src/App.js
+++ src/App.js
@@ -1,53 +1,83 @@
-import React, {useState, useRef} from 'react'
+import React, { useState, useRef } from 'react'
+import * as THREE from 'three'
 import './style.css'
-import { Canvas, useFrame } from 'react-three-fiber'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { Canvas, extend, useThree, useFrame } from 'react-three-fiber'
 import { useSpring, a } from 'react-spring/three'
 
 function App() {
 
+    extend({OrbitControls})
+
+    const Controls = () => {
+        const {
+            camera,
+            gl: { domElement },
+        } = useThree()
+        const orbitRef = useRef()
+        useFrame((state) => orbitRef.current.update())
+        return (
+            <orbitControls autoRotate
+                maxPolarAngle = {Math.PI/ 3}
+                maxPolarAngle = {Math.PI/ 3}
+                ref={orbitRef}
+                args={[camera, domElement]}
+            />
+        )
+    }
+
+    const Plane = () => {
+        <mesh rotation = {[Math.PI/ 2, 0, 0]} position = {[0, -0.5, 0]} recieveShadow>
+            <planeBufferGeometry
+                    attach='geometry'
+                    args = {[100,100]}
+            />
+            <meshPhysicalMaterial
+                attach = 'material'
+                color = 'red'
+            />
+        </mesh>
+    }
+
     const Box = () => {
-        const meshRef = useRef()
         const [hovered, setHover] = useState(false)
         const [active, setActive] = useState(false)
         const props = useSpring({
             scale: active ? [1.5, 1.5, 1.5] : [1, 1, 1],
-            color: (hovered || active) ? 'hotpink' : 'orange',
-        })
-
-        useFrame(() => {
-            meshRef.current.rotation.x = meshRef.current.rotation.y += 0.01
+            color: (hovered || active) ? 'hotpink' : 'gray',
         })
 
         return(
             <a.mesh
-                ref = {meshRef}
                 onPointerOver = {() => setHover(true)}
                 onPointerOut = {() => setHover(false)}
                 onClick = { () => setActive(!active)}
                 scale = {props.scale}
+                castShadow
             >
-                <boxBufferGeometry
-                    attach='geometry'
-                    args = {[1,1,1]}
-                />
-                <a.meshStandardMaterial
-                    attach = 'material'
-                    color = {props.color}
-                />
+                <ambientLight intensity={0.5} />
+                <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} castShadow/>
+                <pointLight position={[-10, -10, -10]} />
+                <boxBufferGeometry attach='geometry' args={[1, 1, 1]} />
+                <a.meshPhysicalMaterial attach='material' color={props.color} />
             </a.mesh>
         )
     }
 
     return (
         <div className="App">
-            <Canvas>
-                <ambientLight intensity={0.5} />
-                <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-                <pointLight position={[-10, -10, -10]} />
+            <Canvas camera={{position: [0, 0, 5]}} onCreated={({ gl}) => {
+                gl.shadowMap.enabled = true
+                gl.shadowMap.type = THREE.PCFSoftShadowMap
+            }}>
+                <fog attach='fog' args={['white', 5, 15]} />
                 <Box position={[0, 0, 0]} />
+                <Controls />
+                <Plane />
             </Canvas>
         </div>
     );
     }
 
 export default App;
+
--- src/test.js
+++ src/test.js
@@ -1,6 +1,39 @@
+
 import React, { useRef, useState } from 'react'
-import { Canvas, useFrame } from 'react-three-fiber'
+import { Canvas, useFrame, extend, useThree } from 'react-three-fiber'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 // import './style.css'
+
+extend({OrbitControls})
+    function Controls() {
+        const {
+            camera,
+            gl: { domElement },
+        } = useThree()
+        const orbitRef = useRef()
+        useFrame((state) => orbitRef.current.update())
+        return (
+            <orbitControls autoRotate
+                maxPolarAngle = {Math.PI/3}
+                maxPolarAngle = {Math.PI/3}
+                ref={orbitRef}
+                args={[camera, domElement]}
+            />
+        )
+    }
+
+function Plane() {
+        <mesh rotate = {[Math.PI/ 2, 0, 0]}>
+            <planeBufferGeometry
+                    attach='geometry'
+                    args = {[100,100]}
+            />
+            <meshPhysicalMaterial
+                attach = 'material'
+                color = 'white'
+            />
+        </mesh>
+    }
 
 function Box(props) {
     // This reference will give us direct access to the mesh
@@ -9,21 +42,20 @@ function Box(props) {
     const [hovered, setHover] = useState(false)
     const [active, setActive] = useState(false)
     // Rotate mesh every frame, this is outside of React without overhead
-    useFrame(() => {
-        mesh.current.rotation.x = mesh.current.rotation.y += 0.01
-    })
 
   return (
     <mesh
       {...props}
-      ref={mesh}
       scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
       onClick={(e) => setActive(!active)}
       onPointerOver={(e) => setHover(true)}
       onPointerOut={(e) => setHover(false)}
     >
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+      <pointLight position={[-10, -10, -10]} />
       <boxBufferGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={(hovered || active) ? 'hotpink' : 'orange'} />
+      <meshPhysicalMaterial color={(hovered || active) ? 'hotpink' : 'orange'} />
     </mesh>
   )
 }
@@ -31,10 +63,10 @@ function Box(props) {
 export default function App() {
   return (
     <Canvas>
-      <ambientLight intensity={0.5} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} />
-      <Box position={[0, 0, 0]} />
+        <Controls />
+        <Box position={[0, 0, 0]} />
+        <Plane />
     </Canvas>
   )
 }
+
