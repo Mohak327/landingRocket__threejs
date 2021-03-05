0517a93600b2ce96feadd07a1eac2db8525b506c
Mohak Sharma <mohak327@gmail.com>
2021-03-05 22:01:53 +0530

    node modules error

--- src/App.js
+++ src/App.js
@@ -1,83 +1,92 @@
-import React, { useState, useRef } from 'react'
 import * as THREE from 'three'
-import './style.css'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { Canvas, extend, useThree, useFrame } from 'react-three-fiber'
-import { useSpring, a } from 'react-spring/three'
+import React, { Suspense, useState, useEffect } from 'react'
+import { Canvas, useLoader } from 'react-three-fiber'
+import { useTransition, a } from 'react-spring'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { OrbitControls, draco } from 'drei'
 
-function App() {
+function Model({ url }) {
+  const { nodes, materials } = useLoader(GLTFLoader, url, draco())
+  return (
+    <group rotation={[-Math.PI / 2, 0, 0]} position={[0, -7, 0]} scale={[7, 7, 7]}>
+      <group rotation={[Math.PI / 13.5, -Math.PI / 5.8, Math.PI / 5.6]}>
+        <mesh castShadow receiveShadow geometry={nodes.planet001.geometry} material={materials.scene} />
+        <mesh castShadow receiveShadow geometry={nodes.planet002.geometry} material={materials.scene} />
+      </group>
+    </group>
+  )
+}
 
-    extend({OrbitControls})
+function Loading() {
+  const [finished, set] = useState(false)
+  const [width, setWidth] = useState(0)
 
-    const Controls = () => {
-        const {
-            camera,
-            gl: { domElement },
-        } = useThree()
-        const orbitRef = useRef()
-        useFrame((state) => orbitRef.current.update())
-        return (
-            <orbitControls autoRotate
-                maxPolarAngle = {Math.PI/ 3}
-                maxPolarAngle = {Math.PI/ 3}
-                ref={orbitRef}
-                args={[camera, domElement]}
-            />
-        )
-    }
+  useEffect(() => {
+    THREE.DefaultLoadingManager.onLoad = () => set(true)
+    THREE.DefaultLoadingManager.onProgress = (url, itemsLoaded, itemsTotal) =>
+      setWidth((itemsLoaded / itemsTotal) * 200)
+  }, [])
 
-    const Plane = () => {
-        <mesh rotation = {[Math.PI/ 2, 0, 0]} position = {[0, -0.5, 0]} recieveShadow>
-            <planeBufferGeometry
-                    attach='geometry'
-                    args = {[100,100]}
-            />
-            <meshPhysicalMaterial
-                attach = 'material'
-                color = 'red'
-            />
-        </mesh>
-    }
+  const props = useTransition(finished, null, {
+    from: { opacity: 1, width: 0 },
+    leave: { opacity: 0 },
+    update: { width },
+  })
 
-    const Box = () => {
-        const [hovered, setHover] = useState(false)
-        const [active, setActive] = useState(false)
-        const props = useSpring({
-            scale: active ? [1.5, 1.5, 1.5] : [1, 1, 1],
-            color: (hovered || active) ? 'hotpink' : 'gray',
-        })
+  return props.map(
+    ({ item: finished, key, props: { opacity, width } }) =>
+      !finished && (
+        <a.div className="loading" key={key} style={{ opacity }}>
+          <div className="loading-bar-container">
+            <a.div className="loading-bar" style={{ width }} />
+          </div>
+        </a.div>
+      ),
+  )
+}
 
-        return(
-            <a.mesh
-                onPointerOver = {() => setHover(true)}
-                onPointerOut = {() => setHover(false)}
-                onClick = { () => setActive(!active)}
-                scale = {props.scale}
-                castShadow
-            >
-                <ambientLight intensity={0.5} />
-                <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} castShadow/>
-                <pointLight position={[-10, -10, -10]} />
-                <boxBufferGeometry attach='geometry' args={[1, 1, 1]} />
-                <a.meshPhysicalMaterial attach='material' color={props.color} />
-            </a.mesh>
-        )
-    }
-
-    return (
-        <div className="App">
-            <Canvas camera={{position: [0, 0, 5]}} onCreated={({ gl}) => {
-                gl.shadowMap.enabled = true
-                gl.shadowMap.type = THREE.PCFSoftShadowMap
-            }}>
-                <fog attach='fog' args={['white', 5, 15]} />
-                <Box position={[0, 0, 0]} />
-                <Controls />
-                <Plane />
-            </Canvas>
-        </div>
-    );
-    }
-
-export default App;
-
+export default function App() {
+  return (
+    <>
+      <div className="bg" />
+      <h1>
+        LEARN
+        <br />
+        <span>w/JASON</span>
+      </h1>
+      <Canvas shadowMap camera={{ position: [0, 0, 15] }}>
+        <ambientLight intensity={0.75} />
+        <pointLight intensity={1} position={[-10, -25, -10]} />
+        <spotLight
+          castShadow
+          intensity={2.25}
+          angle={0.2}
+          penumbra={1}
+          position={[25, 25, 25]}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-bias={-0.0001}
+        />
+        <fog attach="fog" args={['#cc7b32', 16, 20]} />
+        <Suspense fallback={null}>
+          <Model url="/public/scene.gltf" />
+        </Suspense>
+        <OrbitControls
+          autoRotate
+          enablePan={false}
+          enableZoom={false}
+          enableDamping
+          dampingFactor={0.5}
+          rotateSpeed={1}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+      </Canvas>
+      <div className="layer" />
+      <Loading />
+      <a href="https://github.com/drcmda/learnwithjason" className="top-left" children="Github" />
+      <a href="https://twitter.com/0xca0a" className="top-right" children="Twitter" />
+      <a href="https://github.com/drcmda/react-three-fiber" className="bottom-left" children="+ react-three-fiber" />
+    </>
+  )
+}
